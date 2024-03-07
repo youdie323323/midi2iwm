726c139f696eee7e01037fcc56bcdb6bad96c930
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2024-03-07 09:20:13 +0900

    de

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-'use client'
-
+'use client';
 import { useEffect } from 'react';
 
 declare const Go: any;
 
 export default function App() {
   useEffect(() => {
-    const runWasm = async () => {
+    (async () => {
       const go = new Go();
       const result = await WebAssembly.instantiateStreaming(fetch('main.wasm'), go.importObject);
       go.run(result.instance);
-    };
-    runWasm();
+    })();
   }, []);
 
   return <main></main>
--- wasm/main.go
+++ wasm/main.go
@@ -1,5 +1,20 @@
+//go:build js && wasm
+
 package main
 
+import (
+	"syscall/js"
+)
+
+var (
+	window   = js.Global()
+	document = window.Get("document")
+)
+
 func main() {
-	println("hihi")
+	window.Set("Image", js.FuncOf(func(this js.Value, args []js.Value) any {
+		return "a"
+	}))
+
+	select {}
 }
