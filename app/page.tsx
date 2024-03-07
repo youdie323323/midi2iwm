b6b454d5dd60184332149233db1554a780d4f30e
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2024-03-07 16:03:47 +0900

    aassa

--- app/page.tsx
+++ app/page.tsx
@@ -149,7 +149,7 @@ export default function App() {
                   aria-describedby="file_input_help"
                   accept=".midi,.mid"
                   onInput={function (e) {
-                    const infoJson = prompt(`Please input your info with JSON (duck Example (see the console table): [{"Index":0,"Volume":1,"PlayKey":0,"PlayKeyPitchStandard":61,"PlayKeyHighestPitch":73,"Offset":0}]  )`)
+                    const infoJson = prompt(`Please input your info with JSON (duck Example (see the console table): [{"Index":0,"Volume":1,"PlayKey":18,"PlayKeyPitchStandard":30,"PlayKeyHighestPitch":49,"Offset":0}] )`)
                     const AppendMapIdx = prompt(`Please specify map index you wanna append`)
 
                     function _arrayBufferToBase64(buffer: any): string {
