18b98cee2bad6317a4fc028235db32d588f620f1
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2024-03-07 10:06:21 +0900

    ss

--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,6 @@ export default function App() {
         </div>
       </div>
       <Script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js' />
-      <Script src='https://cdn.jsdelivr.net/npm/apexcharts@3.26.3/dist/apexcharts.min.js' />
     </div>
   )
 }
