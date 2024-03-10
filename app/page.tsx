fd27022e60eb3da02f0ca2083c56d377d8320c4a
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2024-03-10 23:12:17 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function App() {
           type="file" />
 
         <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">Please input your map file before, *.map file only</p>
-        <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">Map file path is \"C:\\Users\\yourUsername\\AppData\\Local\\IWM\\maps\"</p>
+        <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">Map file path is &quot;C:\Users\yourUsername\AppData\Local\IWM\maps&quot;</p>
 
         <div className="row row-cols-1 row-cols-md-2 g-4">
           <div className="col-md-6">
