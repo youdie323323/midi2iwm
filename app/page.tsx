e1c2c7308467e605ded18d7305baa566785ee7b7
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2024-03-11 13:41:21 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -220,7 +220,7 @@ export default function App() {
                   </i>{" "}
                   Midi to IWM
                   <span className="ms-auto" style={{ color: "#666" }}>
-                    UnStable
+                    Stable
                   </span>
                 </h3>
                 <p className="card-text">
@@ -259,7 +259,7 @@ export default function App() {
                   rows={28}
                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder={`
-Example of convert midi track number 0, 1 to IWM
+Example of convert midi track number 0, 1 with piano to IWM
 [
   {
     "Index":0,
