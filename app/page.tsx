d1f6d462c5c01625ae51e5465c91f36f4d13fc3b
youdie323323 <131268899+youdie323323@users.noreply.github.com>
2024-03-11 13:21:46 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -255,7 +255,7 @@ export default function App() {
                   id="JSONtrackInfo"
                   rows={10}
                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="テキスト1&#10;テキスト2&#10;テキスト3"
+                  placeholder="テキスト1&#13;&#10;asssa"
                 ></textarea>
               </div>
             </div>
