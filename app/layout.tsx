c607b0cb49962a6564e84698721fabcf17c9db90
youdi3 <131268899+youdie323323@users.noreply.github.com>
2024-06-23 06:04:57 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script strategy='beforeInteractive' src='wasm_exec.js' />
+        <Script strategy='beforeInteractive' src='wasm_exec.js'type="module" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"></link>
       </head>
